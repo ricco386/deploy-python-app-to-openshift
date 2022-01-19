a983c9ebae5a54f69fff249290814cfe7bdc2f34
Richard Kellner <richard.kellner@gmail.com>
2022-01-19 18:44:46 +0100

    Added allowed host

--- meetup/settings.py
+++ meetup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4wpg*t3^f6pt04r_v1-(fcjj-n3us7&skgl=fbi8+3f_ov&fbz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
